7192410d948f46b5ed81625eababba58c94f0224
KKumarPro <ak23071977@gmail.com>
2026-01-17 22:34:53 +0530

    Getter Setter Encapsulation Method

---
+++ java-foundation/src/GetterSetter.java
@@ -0,0 +1,25 @@
+class Myself{
+    private int age;
+    private String name;
+
+    public void setAge(int a){
+        age = a;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public String getName(){
+        return name;
+    }
+}
+public class GetterSetter {
+    public static void main(String[] a){
+        Myself obj = new Myself();
+        obj.setAge(11);
+        obj.setName("Karan");
+        System.out.println(obj.getName()+" : "+obj.getAge());
+    }
+}
