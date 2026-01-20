f7540df6334b8c9bb22ac4f7d76b3f18afa3cc4f
KKumarPro <ak23071977@gmail.com>
2026-01-20 22:29:10 +0530

    End of file logic sloved

--- java-foundation/src/EndOfFile.java
+++ java-foundation/src/EndOfFile.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class EndOfFile {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lineNumber = 1;
+        while (sc.hasNextLine()){
+            String line = sc.nextLine();
+            System.out.println(lineNumber+ " " +line);
+            lineNumber++;
+        }
+        sc.close();
+    }
 }
