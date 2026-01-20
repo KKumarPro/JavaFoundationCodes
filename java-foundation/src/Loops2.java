6a1902b999798dd4722483994d7ddd1841fdc4c7
KKumarPro <ak23071977@gmail.com>
2026-01-20 21:14:00 +0530

    Loops problem sloved

---
+++ java-foundation/src/Loops2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.*;
+
+class Loops2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int q = sc.nextInt();
+
+        for (int i = 0; i < q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+
+            int sum = a;
+
+            for (int j = 0; j < n; j++) {
+                sum += (1 << j) * b;   // 2^j * b
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
