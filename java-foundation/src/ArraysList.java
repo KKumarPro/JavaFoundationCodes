5be5c6509d793eb524dfe3a4cb12e205c4449e48
KKumarPro <ak23071977@gmail.com>
2026-01-21 23:11:28 +0530

    ArrayList example sloved

---
+++ java-foundation/src/ArraysList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArraysList {
+    public static void main(String[] a){
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10,20,30,40,50));
+        System.out.println("Before: "+list);
+
+        list.remove(2);
+        System.out.println("After: "+list);
+    }
+}
