dcf8030f344fd90fdd5fc6b3803c13b07b16f2e7
KKumarPro <ak23071977@gmail.com>
2026-01-25 23:22:02 +0530

    Set implementation

---
+++ java-foundation/src/Sets.java
@@ -0,0 +1,18 @@
+//Example of Set
+
+import java.util.*;
+public class Sets {
+    public static void main(String[] a){
+        Set<Integer> s = new HashSet<>();
+        s.add(11); s.add(45); s.add(11); s.add(45); s.add(40);
+        System.out.println(s);
+
+        Set<Integer> s2 = new LinkedHashSet<>();
+        s2.add(11); s2.add(45); s2.add(11); s2.add(45); s2.add(40);
+        System.out.println(s2);
+
+        Set<Integer> s3 = new TreeSet<>();
+        s3.add(11); s3.add(45); s3.add(11); s3.add(45); s3.add(40);
+        System.out.println(s3);
+    }
+}
