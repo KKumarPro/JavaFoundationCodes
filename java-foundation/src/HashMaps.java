097b5bcd98c7227ddd1801cc9efa9b3a6f22333c
KKumarPro <ak23071977@gmail.com>
2026-01-27 22:32:33 +0530

    Hash Map Methods implementation

--- java-foundation/src/HashMaps.java
+++ java-foundation/src/HashMaps.java
@@ -7,7 +7,17 @@ public class HashMaps {
         register.put(1, "Karan");
         register.put(2, "Ram");
         register.put(3, "Shyam");
+
         System.out.println(register);
         System.out.println(register.get(1)+" "+register.get(2));
+
+        for(int i : register.keySet())
+            System.out.println("ID: "+i+"   "+"Name: "+register.get(i));
+
+        register.remove(3);
+        System.out.println(register);
+        System.out.println(register.size());
+        register.clear();
+        System.out.println(register);
     }
 }
