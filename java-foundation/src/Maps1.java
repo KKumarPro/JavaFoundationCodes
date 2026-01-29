3837e40183f5a92279e3beec0c4b5e875dc255d3
KKumarPro <ak23071977@gmail.com>
2026-01-29 22:34:05 +0530

    Map example 1 implementation

---
+++ java-foundation/src/Maps1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Maps1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        Map<String, String> phoneBook = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String name = sc.nextLine();
+            String phone = sc.nextLine();
+            phoneBook.put(name, phone);
+        }
+
+        while (sc.hasNext()) {
+            String query = sc.nextLine();
+            if (phoneBook.containsKey(query)) {
+                System.out.println(query + "=" + phoneBook.get(query));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
