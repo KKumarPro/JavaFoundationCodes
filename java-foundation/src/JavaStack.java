823e05c823a29abb08bf34e207aeb3b566f5c02f
KKumarPro <ak23071977@gmail.com>
2026-01-29 22:46:52 +0530

    Java Stack example 1 implementation

---
+++ java-foundation/src/JavaStack.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class JavaStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            Stack<Character> stack = new Stack<>();
+            boolean balanced = true;
+
+            for (char ch : s.toCharArray()) {
+                if (ch == '(' || ch == '{' || ch == '[') {
+                    stack.push(ch);
+                } else {
+                    if (stack.isEmpty()) {
+                        balanced = false;
+                        break;
+                    }
+                    char top = stack.pop();
+                    if ((ch == ')' && top != '(') ||
+                            (ch == '}' && top != '{') ||
+                            (ch == ']' && top != '[')) {
+                        balanced = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!stack.isEmpty()) balanced = false;
+
+            System.out.println(balanced);
+        }
+    }
+}
