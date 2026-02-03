36b02b8c65770a0571719cd8a17c96665e39417a
KKumarPro <ak23071977@gmail.com>
2026-02-03 23:41:06 +0530

    Array1 Started

--- java-foundation/src/DSA/Array1.java
+++ java-foundation/src/DSA/Array1.java
@@ -4,8 +4,8 @@ public class Array1 {
     public static void main(String[] a){
         int[] arr = {1,3,9,23,7};
         int sum = 0;
-        for (int j : arr) {
-            sum = sum + j;
+        for (int i : arr) {
+            sum = sum + i;
         }
         System.out.println(sum);
     }
