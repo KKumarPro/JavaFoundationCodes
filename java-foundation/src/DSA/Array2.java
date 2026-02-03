038a5512071e21fa4d8c5f32d9a23ccdf73ddf5f
KKumarPro <ak23071977@gmail.com>
2026-02-03 22:27:53 +0530

    Array Started

--- java-foundation/src/DSA/Array2.java
+++ java-foundation/src/DSA/Array2.java
@@ -4,11 +4,16 @@ public class Array2 {
     public static void main(String[] a){
         int[] arr = {76,23,87,4,97,34};
         int max = arr[0];
+        int min = arr[0];
         for (int i : arr) {
             if (i > max) {
                 max = i;
             }
+            if(i < min){
+                min = i;
+            }
         }
         System.out.println("Maximum: "+max);
+        System.out.println("Minimum: "+min);
     }
 }
