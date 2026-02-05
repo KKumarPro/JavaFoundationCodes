cd5497759bab247989c81ef0396a4a107c102e9b
KKumarPro <ak23071977@gmail.com>
2026-02-05 22:41:02 +0530

    Array test page

--- java-foundation/src/DSA/Array8.java
+++ java-foundation/src/DSA/Array8.java
@@ -5,20 +5,24 @@ public class Array8 {
     public static void main(String[] a) {
         Scanner sc = new Scanner(System.in);
 
+        //Taking input of number of elements in the array
         System.out.print("Enter the number of elements: ");
         int size = sc.nextInt();
         int[] arr1 = new int[size];
 
+        //Taking input of the elements of the array
         System.out.print("Enter the elements of the array: ");
         for(int i = 0; i < arr1.length; i++){
                 arr1[i] = sc.nextInt();
         }
 
+        //Printing the array
         System.out.print("Array: ");
         for (int i : arr1) {
             System.out.print(i+" ");
         }
 
+        //Sorting the array
         System.out.print("\nSorted Array: ");
         for(int i = 0; i < arr1.length-1; i++){
             for(int j =0; j < arr1.length-i-1; j++){
@@ -33,6 +37,7 @@ public class Array8 {
             System.out.print(i + " ");
         }
 
+        //Searching the elements of the array after sorting
         System.out.print("\nEnter the element to search: ");
         int key = sc.nextInt();
         int index = -1;
