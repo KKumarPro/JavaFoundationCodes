96cffffaf9e98fb3cb8686a3404844c45a9dbe3c
KKumarPro <ak23071977@gmail.com>
2026-02-06 22:23:07 +0530

    Sorting: Bubble, selection, Insertion sort test

--- java-foundation/src/DSA/Sorting1.java
+++ java-foundation/src/DSA/Sorting1.java
@@ -51,14 +51,14 @@ public class Sorting1 {
 
         //Insertion Sort
         for(int i = 0; i < arr.length; i++){
-            int key = arr[i];
+            int current = arr[i];
             int j = i-1;
 
-            while (j >= 0 && arr[j] > key){
+            while (j >= 0 && arr[j] > current){
                 arr[j+1] = arr[j];
                 j--;
             }
-            arr[j+1] = key;
+            arr[j+1] = current;
         }
         System.out.print("\nInsertion-Sort array: ");
         for(int x : arr){
