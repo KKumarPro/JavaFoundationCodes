f8da5cafea88b30e02bec035ae4408539c7e84b0
KKumarPro <ak23071977@gmail.com>
2026-02-08 23:18:58 +0530

    String test

--- java-foundation/src/DSA/Strings1.java
+++ java-foundation/src/DSA/Strings1.java
@@ -9,10 +9,20 @@ public class Strings1 {
         System.out.println(name);
         System.out.println(name.length());
 
+        //Reverse a string
         String reverse = "";
         for(int i = name.length()-1; i >= 0; i--){
             reverse += name.charAt(i);
         }
         System.out.println(reverse);
+
+        //To Check Palindrome
+        for(int i = name.length()-1; i >= 0; i--){
+            reverse += name.charAt(i);
+        }
+        if (name.equals(reverse))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
     }
 }
