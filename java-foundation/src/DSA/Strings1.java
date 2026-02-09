c5f178b261cf70a9d2f2c1010d8c3175e4d84a5a
KKumarPro <ak23071977@gmail.com>
2026-02-09 20:31:42 +0530

    String test

--- java-foundation/src/DSA/Strings1.java
+++ java-foundation/src/DSA/Strings1.java
@@ -24,5 +24,27 @@ public class Strings1 {
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
+
+        //Vowels and Consonants count
+        int v = 0, c = 0;
+        for(int i = 0; i < name.length(); i++){
+            char ch = name.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                v++;
+            else
+                c++;
+        }
+        System.out.println("Vowels: "+v+" ");
+        System.out.println("Consonants: "+c+" ");
+
+        //Check Frequency
+        int[] freq = new int[256];
+        for (int i = 0; i < name.length(); i++) {
+            freq[name.charAt(i)]++;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0)
+                System.out.println((char)i + " : " + freq[i]);
+        }
     }
 }
