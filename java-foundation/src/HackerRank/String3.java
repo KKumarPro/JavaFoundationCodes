2eed57cba154937280243199fddb311a67330167
KKumarPro <ak23071977@gmail.com>
2026-02-19 22:58:21 +0530

    String Operations Test

--- java-foundation/src/HackerRank/String3.java
+++ java-foundation/src/HackerRank/String3.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 public class String3 {
     public static void main(String[] a){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any word: ");
         String s = sc.nextLine();
+
+        System.out.print("Enter the number of words you want to select: ");
         int k = sc.nextInt();
+
         String smallest = s.substring(0,k);
         String largest = s.substring(0,k);
+
         for(int i=1; i<=s.length()-k; i++){
             String current = s.substring(i, i+k);
             if(current.compareTo(smallest) < 0)
